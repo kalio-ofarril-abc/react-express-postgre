960692744d4f864fed6a604cdbf6d6aa4225ce6a
kalio-ofarril-abc <abcdevelopment.k@gmail.com>
2021-07-27 19:04:15 -0500

    Allowing storyTeling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const story_model = require('./story_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://contact-react-app-heroku.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
